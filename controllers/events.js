cc87af197bc4f9aa490d4aafdf2327d81fef23d1
Maizares, Juan <maizaresjuan.coder@gmail.com>
2022-10-23 20:27:28 -0300

    eliminar evento

--- controllers/events.js
+++ controllers/events.js
@@ -35,7 +35,7 @@ const actualizarEventos = async(req, res = response) => {
     try {
         const evento = await Evento.findById( eventoId );
         if (!evento) {
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'Evento no existente con ese id.'
             });
@@ -67,12 +67,36 @@ const actualizarEventos = async(req, res = response) => {
     
 }
 // mtd: ELIMINAR UN EVENTO 
-const eliminarEventos = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'eliminarEventos '
-    })
+const eliminarEventos = async( req, res = response ) => {
+    const eventoId = req.params.id;
+    const uid = req.uid;
+    try {
+        const evento = await Evento.findById( eventoId );
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento inexistente.'
+            });
+        }
+        if ( evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'Sin permisos para eliminar.'
+            });
+        }
+        await Evento.findByIdAndDelete(eventoId);
+        res.json({ 
+            ok: true 
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Conectarse con un admin.'
+        });
+    }
 }
+
+
 
 module.exports = {
     getEventos,
