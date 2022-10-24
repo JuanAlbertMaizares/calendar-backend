2a1e3d7306b9df30006f5a8518502f9234be7662
Maizares, Juan <maizaresjuan.coder@gmail.com>
2022-10-23 23:52:02 -0300

    .env add

---
+++ local-index-version.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
+const cors = require('cors');
+// process.env.NAME_VAR
+
+// crear app server express
+const app = express();
+//DB
+dbConnection();
+// CORS
+app.use(cors());
+app.use(express.static('public'));
+// note: Leer y obtener/parsear lo que venga en formato json.
+app.use(express.json());
+//note: RUTAS
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
+
+// escuchar solicitudes
+app.listen(process.env.PORT, ()=>{
+    console.log(`servidor corriendo en puerto ${process.env.PORT}`);
+});
